163c37c5b0650d7e016cc1ab18b6fda52bc20703
Aditya REddy <aditya.reddy@hotmail.co.nz>
2018-08-15 10:59:58 +1200

    GruntFile

---
+++ GruntFile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css/',
+          ext: '.min.css'
+        }]
+      }
+    },
+  });
+}
